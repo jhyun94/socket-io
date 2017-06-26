29ebafccb7ce026a58d24bd02fc1d709e3c9d1de
jhyun94 <hyunjonathan@gmail.com>
2017-06-26 14:26:14 -0500

    added socket to client and server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 var port = process.env.PORT;
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log('new user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  })
+})
 
 
-app.listen(port || 3000, () => {
+server.listen(port || 3000, () => {
   console.log('You are listening on port 3000');
 })
