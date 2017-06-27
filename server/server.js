260de22bf60c3fc68f20e7d6cc722c7cbb81a420
jhyun94 <hyunjonathan@gmail.com>
2017-06-27 15:21:44 -0500

    wrote message utils and tested the function

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 var app = express();
 var server = http.createServer(app);
